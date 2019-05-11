556350a7411800d50adf609fdef3c507776108e2
GradyDeReamer7602 <50172539+GradyDeReamer7602@users.noreply.github.com>
2019-05-10 21:31:01 -0400

    hi

--- function-review.js
+++ function-review.js
@@ -1,9 +1,29 @@
 //Read questions in the README and answer them here
 
 //Q.1 code here
+Type1 function(){
 
+}
+
+Type2 var now = function(){
+
+}
 //Q.2 code here
-
+definition:
+var text = "5 + 3"
+var returnFivePlusThree= function(){
+	return text
+}
+invocation:
+returnFivePlusThree();
 //Q.3 code here
-
+function returnArg (arg1){
+	return arg1
+}
+returnArg('five');
 //Q.4 code here
+var returnVar = function(){
+	var returnVar = ("questions")
+	return(returnVar)
+}
+returnVar();
